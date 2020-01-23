034fdb3ee704bdce68d2eda4a9009265a82815da
Adnan Niloy <apache_adnan1313@hotmail.com>
2020-01-24 04:21:53 +0600

    Init

--- TestDoc/Controllers/HomeController.cs
+++ TestDoc/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.Office.Interop.Word;
 using System.Web.Mvc;
+using TestDoc.Models;
 
 namespace TestDoc.Controllers
 {
@@ -25,6 +23,45 @@ namespace TestDoc.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public void ExportToWord(DocProperties model)
+        {
+            var savePath = Server.MapPath("~/Docs/PersonInfo.docx");
+            var templatePath = Server.MapPath("~/Docs/Template.docx");
+            var app = new Application();
+            var doc = app.Documents.Open(templatePath);
+            doc.Activate();
+
+            if (doc.Bookmarks.Exists("RNO"))
+            {
+                doc.Bookmarks["RNO"].Range.Text = model.Rno;
+            }
+            if (doc.Bookmarks.Exists("Code"))
+            {
+                doc.Bookmarks["Code"].Range.Text = model.Code;
+            }
+            if (doc.Bookmarks.Exists("Description"))
+            {
+                doc.Bookmarks["Description"].Range.Text = model.Description;
+            }
+
+            doc.SaveAs2(savePath);
+            doc.Close();
+
+            //byte[] fileBytes = System.IO.File.ReadAllBytes(savePath);
+            //string fileName = "MyFile.docx";
+            //return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+
+            Response.ContentType = "application/docx";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=SailBig.docx");
+            Response.TransmitFile(savePath);
+            Response.End();
+
+            if (System.IO.File.Exists(savePath))
+            {
+                System.IO.File.Delete(savePath);
+            }
         }
     }
 }
---
+++ TestDoc/Models/DocProperties.cs
@@ -0,0 +1,11 @@
+﻿namespace TestDoc.Models
+{
+    public class DocProperties
+    {
+        public string Rno { get; set; }
+
+        public string Code { get; set; }
+
+        public string Description { get; set; }
+    }
+}
